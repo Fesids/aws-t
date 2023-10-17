35840c98a1eef21619748f6e8a97f53e11da3bfa
Fesids <116668620+Fesids@users.noreply.github.com>
2023-10-17 19:00:52 -0300

    Update MySQLDBConnect.ts

--- src/DATABASE/MySQLDBConnect.ts
+++ src/DATABASE/MySQLDBConnect.ts
@@ -1,7 +1,7 @@
 import mysql2, {Pool} from "mysql2";
 
 const dbConfig = {
-    host: "my-database.cbjjagspaa3y.us-east-1.rds.amazonaws.com",
+    host: "db.cbjjagspaa3y.us-east-1.rds.amazonaws.com",
     database: "js_registration",
     user: "root", 
     password: "67890000"
